bf29a657142ba633e5ce3dc7acb6c385266b8b08
latifshkh <slatif2004262@gmail.com>
2026-07-18 04:16:37 +0530

    fetchleads

--- src/lib/appwrite/server-admin.ts
+++ src/lib/appwrite/server-admin.ts
@@ -2,11 +2,15 @@
 import { Client, Databases } from 'node-appwrite';
 
 // Create client
-const client = new Client()
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
+const client = new Client();
 
+const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
+const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID;
 const apiKey = process.env.APPWRITE_API_KEY;
+
+if (endpoint && projectId) {
+  client.setEndpoint(endpoint).setProject(projectId);
+}
 
 if (apiKey) {
   client.setKey(apiKey);
